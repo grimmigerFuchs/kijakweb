6e0078407f89abed7d5ce5f9514b9c49afcf8c1a
grimmigerFuchs <grimmigerfuchs@gmail.com>
2021-03-03 16:54:53 +0100

    Added "coffe" task

--- gulpfile.js
+++ gulpfile.js
@@ -6,50 +6,59 @@ var autoprefixer = require("gulp-autoprefixer");
 var concat = require("gulp-concat");
 var csso = require("gulp-csso");
 var minify = require("gulp-minify-css");
+var coffee = require("gulp-coffee");
 var htmlmin = require("gulp-htmlmin");
 
 
 // builds
-gulp.task("less", function () {
-  return gulp.src("./src/less/**/*.less")
-    .pipe(less())
-    .pipe(gulp.dest("./dist/css"));
+gulp.task("less", function() {
+    return gulp.src("./src/less/**/*.less")
+        .pipe(less())
+        .pipe(gulp.dest("./dist/css"));
 });
 
-gulp.task("css", function () {
-  return gulp.src("./src/css/**/*.css")
-    .pipe(autoprefixer())
-    .pipe(csso())
-    .pipe(gulp.dest("./dist/css"))
+gulp.task("css", function() {
+    return gulp.src("./src/css/**/*.css")
+        .pipe(autoprefixer())
+        .pipe(csso())
+        .pipe(gulp.dest("./dist/css"))
+});
+
+gulp.task("coffee", function() {
+    return gulp.src("./src/coffee/**/*.coffee")
+        .pipe(coffee({bare: true}))
+        .pipe(gulp.dest("./dist/js"));
 });
 
 gulp.task("js", function() {
-  return gulp.src("./src/js/**/*.js")
-    .pipe(uglify())
-    .pipe(gulp.dest("./dist/js"))
+    return gulp.src("./src/js/**/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist/js"))
 });
 
 gulp.task("pages", function() {
-  return gulp.src(["./src/*.html"])
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-      removeComments: true
-    }))
-    .pipe(gulp.dest("./dist")),
-    gulp.src(["./src/components/**/*.html"])
-      .pipe(htmlmin({
-        collapseWhitespace: true,
-        removeComments: true
-      }))
-      .pipe(gulp.dest("./dist/components"));
+    return gulp.src(["./src/*.html"])
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest("./dist")),
+        gulp.src(["./src/components/**/*.html"])
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest("./dist/components"));
 });
 
 // meta
-gulp.task("build", gulp.parallel("less", "css", "js", "pages"));
+gulp.task("build", gulp.parallel("less", "css", "coffee", "js", "pages"));
 
 gulp.task("watch", function() {
     gulp.watch("./src/less/**/*.less", gulp.parallel("less"));
     gulp.watch("./src/css/**/*.css", gulp.parallel("css"));
+    gulp.watch("./src/cofee/**/*.coffee", gulp.parallel("coffee"));
+    gulp.watch("./src/js/**/*.js", gulp.parallel("js"));
     gulp.watch("./src/**/*.html", gulp.parallel("pages"));
 });
 
