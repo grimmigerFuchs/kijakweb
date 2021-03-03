fc0fc0ed1b92281dc5ceb1e424e603194c54610e
grimmigerFuchs <grimmigerfuchs@gmail.com>
2021-03-03 15:10:36 +0100

    Added css minification task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
 var path = require("path");
+var uglify = require("gulp-uglify");
+var autoprefixer = require("gulp-autoprefixer");
+var concat = require("gulp-concat");
+var csso = require("gulp-csso");
+var minify = require("gulp-minify-css");
 
 gulp.task("less", function () {
   return gulp.src("./less/style.less")
@@ -8,4 +13,11 @@ gulp.task("less", function () {
       paths: [ path.join(__dirname, "less", "includes") ]
     }))
     .pipe(gulp.dest("./css"));
+});
+
+gulp.task("css", function () {
+  return gulp.src("./css/style.css")
+    .pipe(autoprefixer())
+    .pipe(csso())
+    .pipe(gulp.dest("./css"))
 });
