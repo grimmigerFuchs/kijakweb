106480489a6c8c430a7161967b0391a7fa1bea6a
grimmigerFuchs <grimmigerfuchs@gmail.com>
2021-03-03 15:21:01 +0100

    Added html minify task

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,34 @@ var autoprefixer = require("gulp-autoprefixer");
 var concat = require("gulp-concat");
 var csso = require("gulp-csso");
 var minify = require("gulp-minify-css");
+var htmlmin = require("gulp-htmlmin");
 
 gulp.task("less", function () {
-  return gulp.src("./src/less/style.less")
+  return gulp.src("./src/less/**/*.less")
     .pipe(less({
       paths: [ path.join(__dirname, "less", "includes") ]
     }))
-    .pipe(gulp.dest("./src/css"));
+    .pipe(gulp.dest("./dist/css"));
 });
 
 gulp.task("css", function () {
-  return gulp.src("./src/css/style.css")
+  return gulp.src("./src/css/**/*.css")
     .pipe(autoprefixer())
     .pipe(csso())
-    .pipe(gulp.dest("./src/css"))
+    .pipe(gulp.dest("./dist/css"))
+});
+
+gulp.task("js", function() {
+  return gulp.src("./src/js/**/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"))
+});
+
+gulp.task("html", function() {
+  return gulp.src(["./src/**/*.html"])
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest("./dist"));
 });
