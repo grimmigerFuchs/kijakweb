61686d45f181eed244a328cb58a5e342e6ec7fa5
grimmigerFuchs <grimmigerFuchs@gmail.com>
2021-03-04 12:53:49 +0100

    Added "clean" task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+// gulpfile.js
+
 var gulp = require("gulp");
 var less = require("gulp-less");
 var path = require("path");
@@ -8,6 +10,7 @@ var csso = require("gulp-csso");
 var minify = require("gulp-minify-css");
 var coffee = require("gulp-coffee");
 var htmlmin = require("gulp-htmlmin");
+var clean = require("gulp-clean");
 
 
 // builds
@@ -32,13 +35,7 @@ gulp.task("coffee", function() {
         .pipe(gulp.dest("./dist/js"));
 });
 
-gulp.task("js", function() {
-    return gulp.src("./src/js/**/*.js")
-        .pipe(uglify())
-        .pipe(gulp.dest("./dist/js"));
-});
-
-gulp.task("pages", function() {
+gulp.task("html", function() {
     return gulp.src(["./src/*.html"])
         .pipe(htmlmin({
             collapseWhitespace: true,
@@ -54,10 +51,17 @@ gulp.task("pages", function() {
 });
 
 // meta
-gulp.task("build", gulp.parallel("less", "css", "coffee", "js", "pages"));
+gulp.task("clean", function() {
+    return gulp.src("./dist/**/*", {read: false})
+    .pipe(clean());
+});
+
+gulp.task("build", gulp.series(
+    "clean", gulp.parallel("less", "css", "coffee", "html"))
+);
 
 gulp.task("watch", function() {
-    gulp.watch("./src", gulp.parallel("build"))
+    gulp.watch("./src", gulp.parallel("build"));
 });
 
 // default
